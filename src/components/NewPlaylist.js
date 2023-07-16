7f9b0972e1372f197c4339a9533904c22f237bce
Nico <nicopasquariello@gmail.com>
2023-07-16 17:47:08 -0600

    added style to form components.

--- src/components/NewPlaylist.js
+++ src/components/NewPlaylist.js
@@ -1,8 +1,15 @@
+import { Button, Textarea } from "@mui/joy";
 import React from "react";
+import '../styles/form.css'
 
 function NewPlaylist({display}){
     return (
         <div className="formContainer" style={{display:display}}>
+            <form  className="createForm">
+                <Textarea placeholder="Playlist Name"/>
+                <Button type="submit" sx={{marginTop:"10px"}}>Submit</Button>
+                <Button /*onClick={closeForm} */ sx={{marginLeft:"10px"}}>Close</Button>
+            </form>
             
         </div>
     )

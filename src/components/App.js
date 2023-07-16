63162b8467950fd6323275ddada2d1f4f41b247f
Nico <nicopasquariello@gmail.com>
2023-07-16 17:15:59 -0600

    playlist names are displayed.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
-import { Button, Container, Typography } from "@mui/joy";
-import React from "react";
+import { Button, Container } from "@mui/joy";
+import React, { useEffect, useState } from "react";
 import PlaylistDisplay from "./PlaylistDisplay";
 import SongDisplay from "./SongDisplay";
 
 function App(){
+    const [playlistData, setPlaylistData] = useState([]);
+    const playlistArr = playlistData.map(playlist => playlist.playlist);
     const buttonSx = {
         position:"relative",
         top:'14vh',
         margin:'5px'
     }
+
+    useEffect(() => {
+        fetch('http://localhost:9292/playlists')
+        .then(r => r.json())
+        .then(data => setPlaylistData(data))
+    }, [])
+
 return (
     <Container sx={{border:"1px solid black"}}>
         <Button sx={buttonSx}>Create Playlist</Button>
         <Button sx={buttonSx}>Add Song</Button>
         <SongDisplay/>
-        <PlaylistDisplay/>
+        <PlaylistDisplay playlistArr={playlistArr}/>
     </Container>
 )
 }
--- src/components/PlaylistDisplay.js
+++ src/components/PlaylistDisplay.js
@@ -2,9 +2,18 @@ import { Typography } from "@mui/joy";
 import React from "react";
 
 function PlaylistDisplay({playlistArr}){
+    const playlistLi = playlistArr.map(playlist => {
+        return (
+            <li key={playlist.id}>
+                <Typography level='h5'>{playlist.name}</Typography>
+            </li>
+        )
+    })
     return (
         <div style={{border:"1px solid black", height:'40vh', width:'15vw', position:'relative', top:'-25.5vh'}}>
-            <Typography level="h1">PlAYLIST DISPLAY</Typography>
+            <ul style={{listStyle:"none"}}>
+                {playlistLi}
+            </ul>
         </div>
         )
 }

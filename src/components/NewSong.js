918027e07bb39a524cc9302ec9a180dc4b82f029
Nico <nicopasquariello@gmail.com>
2023-07-17 19:27:20 -0600

    Added content to NewSong.js.

--- src/components/NewSong.js
+++ src/components/NewSong.js
@@ -1,11 +1,23 @@
 import React from "react";
 import '../styles/form.css'
+import { Button, Textarea } from "@mui/joy";
 
 function NewSong({display}){
     return (
         <div className="formContainer" style={{display:display}}>
-            
-        </div>
+        <form className="form">
+            <Textarea className="newSongInput" placeholder="Song Title"/>
+            <Textarea className="newSongInput" placeholder="Artist"/>
+            <Textarea className="newSongInput" placeholder="Album"/>
+            <input className="newSongInput" type="url" placeholder="YouTube URL"/>
+            <select className="newSongInput">
+                <option>Select Playlist</option>
+            </select>
+            <Button type="submit" sx={{marginTop:"10px"}}>Submit</Button>
+            <Button sx={{marginLeft:"10px"}}>Close</Button>
+        </form>
+        
+    </div>
     )
 }
 

ec1450c311626c4f78947f0107837fb7117a8eb5
Nico <nicopasquariello@gmail.com>
2023-07-21 14:11:16 -0600

    Added unchangeable values to editSong form.

--- src/components/EditSong.js
+++ src/components/EditSong.js
@@ -3,27 +3,36 @@ import React from "react";
 import '../styles/editSongForm.css'
 
 function EditSong({display, closeEditSong}){
+
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+
+
     return (
         <div id="editSongFormContainer" style={{ display: display }}>
-          <form id="editSongForm">
+          <form onSubmit={handleSubmit} id="editSongForm">
             <Textarea
               name="song_title"
+              value='New Song Title'
               className="newSongInput"
               placeholder="Song Title"
             />
             <Textarea
               name="artist"
+              value='New Artist'
               className="newSongInput"
               placeholder="Artist"
             />
             <Textarea
               name="album"
-
+              value='New Album'
               className="newSongInput"
               placeholder="Album"
             />
             <input
               name="url"
+              value="https://THISisAnewFAKEurl.com"
               className="newSongInput"
               type="url"
               placeholder="YouTube URL"

89f8e128eb4efbea60562e840b84a89ec5483977
Nico <nicopasquariello@gmail.com>
2023-07-26 11:28:33 -0600

    Changed playlist buttons to Links.

--- src/components/PlaylistDisplay.js
+++ src/components/PlaylistDisplay.js
@@ -1,23 +1,53 @@
 import { Typography } from "@mui/joy";
 import React from "react";
+import { Link } from "react-router-dom";
 
-function PlaylistDisplay({playlistArr, openSongs}){
-    const playlistLi = playlistArr.map(playlist => {
-        return (
-            <li onClick={() => openSongs(playlist.id)} className="playlistLi" key={playlist.id}>
-                <Typography level='h5'>{playlist.name}</Typography>
-            </li>
-        );
-    });
-
+function PlaylistDisplay({ playlistArr, openSongs }) {
+    const playlistLinkStyle = {
+        color:"black",
+        fontSize:"20px",
+        textDecoration:"none"
+    }
+  const playlistLi = playlistArr.map((playlist) => {
     return (
-        <div style={{border:"1px solid black", height:'40vh', width:'15vw', position:'relative', top:'-25.5vh'}}>
-            <Typography sx={{borderBottom:'3px solid black'}}level='h4'>Playlists</Typography>
-            <ul style={{listStyle:"none", overflowY:"scroll",height:'35vh', position:'relative', bottom:'1vh'}}>
-                {playlistLi}
-            </ul>
-        </div>
-        )
+      <Link id={playlist.name} style={playlistLinkStyle} to={`/playlists/${playlist.name}`}>
+        <li
+          onClick={() => openSongs(playlist.id)}
+          className="playlistLi"
+          key={playlist.id}
+        >
+          {playlist.name}
+        </li>
+      </Link>
+    );
+  });
+
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        height: "40vh",
+        width: "15vw",
+        position: "relative",
+        top: "-25.5vh",
+      }}
+    >
+      <Typography sx={{ borderBottom: "3px solid black" }} level="h4">
+        Playlists
+      </Typography>
+      <ul
+        style={{
+          listStyle: "none",
+          overflowY: "scroll",
+          height: "35vh",
+          position: "relative",
+          bottom: "1vh",
+        }}
+      >
+        {playlistLi}
+      </ul>
+    </div>
+  );
 }
 
 export default PlaylistDisplay;
